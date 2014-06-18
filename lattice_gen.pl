cfb5f741e776c2b8ab69747c0edc13f610c2f07a
Ben Rickman <rickman.ben@gmail.com>
2014-06-18 17:31:30 -0500

    removed (x,y) postions from output of datafile. Now datafile is a grid of potentials

--- lattice_gen.pl
+++ lattice_gen.pl
@@ -3,26 +3,27 @@
 use strict;
 use warnings;
 
-my $step = 0.1;
-my $xlen = 1;
-my $ylen = 1;
+my $step = 1;
+my ($xi,$xf) = (0,100);
+my ($yi,$yf) = (0,100);
 
 my $debug = 0;
+my $dec = 3;
 
 sub func {
   my ($x,$y) = @_;
-  my $func = $x + exp(-$y**2);
+  my $func = $x + $y/1000;
   return $func unless $debug;
   return ($func,3*$x**2 * $y**4,4*$x**3 * $y**3);
 }
 
-open my $FH, "> lattice.dat";
+open my $FH, "> lattice_$xi\_$xf\_$step\_$yi\_$yf\_$step.dat";
 
-for my $x (-$xlen/$step-1..$xlen/$step+1) {
-  $x *= $step;
-  for my $y (-$ylen/$step-1..$ylen/$step+1) {
-    $y *= $step;
-    print $FH (join (", ", ($x,$y,func($x,$y))) . "\n");
+for my $x (-1..($xf-$xi)/$step+1) {
+  $x = $xi + $x*$step;
+  for my $y (-1..($yf-$yi)/$step+1) {
+    $y = $yi + $y*$step;
+     print $FH sprintf("%2.${dec}f", func($x,$y)) . ", ";
   }
   print $FH "\n";
 }
